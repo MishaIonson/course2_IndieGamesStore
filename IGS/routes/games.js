ad25cbbb2e1519c3b62008411123b122fdd0c5e0
Mykhailo Ivanov <mykhivanov@gmail.com>
2016-11-14 18:24:00 +0200

    structure update

--- IGS/routes/games.js
+++ IGS/routes/games.js
@@ -4,7 +4,6 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
 
-  // @TODO: use the same blank for search and top
   res.render('top');
 });
 
--- IGS/routes/search.js
+++ IGS/routes/search.js
@@ -5,10 +5,21 @@ var router = express.Router();
 router.get('/*', function(req, res, next) {
 
   // console.log("Searched: " + req.query.search_line);
+  var feed_posts = [];
 
-  
+  // @TODO: put useful data
+  for (var i = 0; i < 10; i++)
+  {
+    feed_posts.push({
+      href: "search",
+      imagePath: "empty.jpg",
+      title: "BethesdaGames",
+      description:"American video game developer, acting as in-house development team for parent company Bethesda Softworks, established in 2001. Previously synonymous with parent publisher Bethesda Softworks, the studio's name and logo were established with the release of The Elder Scrolls III: Morrowind in 2002. The studio is currently led by executive producer Todd Howard. A second studio, Bethesda Game Studios Montreal, was opened in Montreal, Canada."
+    });
+  }
 
-  res.render('search');
+
+  res.render('search', {feed_posts: feed_posts});
 });
 
 module.exports = router;
